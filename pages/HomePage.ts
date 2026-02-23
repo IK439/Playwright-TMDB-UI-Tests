6a6a996836febd05e0654505eac8ca9e0a24e614
Irfan K <irfan@Irfans-MacBook-Pro.local>
2026-02-23 18:42:47 +0000

    Updated home POM and spec

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -3,35 +3,43 @@ import { BasePage } from "./BasePage";
 import { ENV } from "../utils/env";
 
 export class HomePage extends BasePage {
+  readonly searchBox: Locator;
+  readonly trending: Locator;
+  readonly latestTrailers: Locator;
+  readonly whatsPopular: Locator;
+  readonly freeToWatch: Locator;
+  readonly joinToday: Locator;
+  readonly leaderBoard: Locator;
+
   readonly searchInput: Locator;
   readonly searchResults: Locator;
   readonly searchItem: Locator;
 
   constructor(page: Page) {
     super(page);
+
+    this.searchBox = page.getByPlaceholder(
+      "Search for a movie, tv show, person......",
+    );
+    this.trending = page.getByRole("heading", {
+      name: "Trending",
+    });
+    this.latestTrailers = page.getByRole("heading", {
+      name: "Latest Trailers",
+    });
+    this.whatsPopular = page.getByRole("heading", { name: "What's Popular" });
+    this.freeToWatch = page.getByRole("heading", { name: "Free To Watch" });
+    this.joinToday = page.getByRole("heading", { name: "Join Today" });
+    this.leaderBoard = page.getByRole("heading", { name: "Leaderboard" });
     this.searchInput = page.getByPlaceholder(
       "Search for a movie, tv show, person......",
     );
+
     this.searchResults = page.getByRole("heading", { name: "Search Results" });
     this.searchItem = page.locator("a").filter({ hasText: /^Inception$/ });
   }
 
   async navigateToHome() {
     await this.navigate(`${ENV.baseUrl}/`);
-  }
-
-  async searchMovie(movieName: string) {
-    await this.searchInput.fill(movieName);
-    await this.searchInput.press("Enter");
-  }
-
-  async selectMovieFromResults(movieName: string) {
-    const movie = this.searchResults.filter({ hasText: movieName }).first();
-    await movie.click();
-  }
-
-  async searchAndSelectMovie(movieName: string) {
-    await this.searchMovie(movieName);
-    await this.selectMovieFromResults(movieName);
   }
 }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,9 +2,12 @@ import { test, expect } from "../fixtures/base";
 
 test.describe("Home Feature", () => {
   test("Search for movie from homepage", async ({ homePage }) => {
-    homePage.searchMovie("Inception");
-
-    await expect(homePage.searchResults).toBeAttached();
-    await expect(homePage.searchItem).toBeAttached();
+    await expect(homePage.searchBox).toBeAttached();
+    await expect(homePage.trending).toBeAttached();
+    await expect(homePage.latestTrailers).toBeAttached();
+    await expect(homePage.whatsPopular).toBeAttached();
+    await expect(homePage.freeToWatch).toBeAttached();
+    await expect(homePage.joinToday).toBeAttached();
+    await expect(homePage.leaderBoard).toBeAttached();
   });
 });
